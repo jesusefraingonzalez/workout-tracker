b3aa458830e5cf1de8787e97b6bc4402b415ffda
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-02-22 13:37:59 -0500

    change exercise schema to workout schema to include a date

---
+++ models/Workout.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose');
+
+// a workout consists of a day and the exercises done
+// Each exercise consists of a type, name, duration, weight, reps, sets, distance
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        required: "Enter the date: ",
+    },
+    exercises: {
+        type: {
+            type: String,
+            trim: true,
+            required: "Enter the type of exercise: "
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: "Enter an exercise name: "
+        },
+        duration: {
+            type: Number,
+            required: "Enter a duration"
+        },
+        weight: {
+            type: Number,
+        },
+        reps: {
+            type: Number,
+        },
+        sets: {
+            type: Number,
+        },
+        distance: {
+            type: Number,
+        },
+    },
+});
+
+const Workout = model('Workout', workoutSchema);
+
+module.exports = Workout;
